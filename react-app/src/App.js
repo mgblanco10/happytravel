5f17ef19a4863079d6343bc130086d19d7cc3c67
Cindy Leiva <cindyleiva376@gmail.com>
2023-08-31 09:15:40 +0200

    maquetacion de createForm / editForm

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import CardsGuest from './components/CardsGuest';
+import CreateForm from './components/CreateForm';
+import EditForm from './components/EditForm';
 
 function App() {
   return (
     <div>
+       <EditForm />
+      <CreateForm />
       <CardsGuest />
     </div>
   );
