a113d00b5dfb417b89aff81de39482e88780eb16
genesis-nf <genefernandz@gmail.com>
2023-09-07 14:52:24 +0200

    Maquetacion favoritos/Guardados

---
+++ react-app/src/pages/ViewOptions.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../css/ViewOptions.css"
+
+const ViewOptions = ({ viewOption, onViewOptionChange }) => {
+  return (
+    <div className="view-options">
+      <button
+        className={`view-option-btn ${viewOption === 'favorites' ? 'active' : ''}`}
+        onClick={() => onViewOptionChange('favorites')}
+      >
+        Favoritos
+      </button>
+      <button
+        className={`view-option-btn ${viewOption === 'userAdded' ? 'active' : ''}`}
+        onClick={() => onViewOptionChange('userAdded')}
+      >
+        Mis destinos
+      </button>
+    </div>
+  );
+};
+
+export default ViewOptions;
