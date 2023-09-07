4e3944731650ade8f9de6db93949a5f6b241d373
mgblanco10 <mgblanco1001@gmail.com>
2023-09-07 05:27:18 +0200

    se guarda la imagen de profile

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ModelNotFoundException;
 
 class AuthController extends Controller {
     
@@ -66,4 +68,29 @@ class AuthController extends Controller {
     public function user(Request $request) {
         return new UserResource($request->user());
     }
+
+    public function updateAvatar(Request $request)
+{
+    $user = $request->user(); 
+    
+    if (!$user) {
+        return response()->json(['error' => 'No se ha encontrado el usuario autenticado'], 401);
+    }
+
+    if ($request->hasFile('avatar')) {
+        $request->validate([
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $file = $request->file('avatar');
+        $fileName = time() . '_' . $file->getClientOriginalName();
+        $file->storeAs('avatars', $fileName, 'public'); 
+
+        $user->update(['image' => $fileName]);
+
+        return response()->json(['success' => true, 'message' => 'Avatar actualizado exitosamente']);
+    }
+
+    return response()->json(['success' => false, 'message' => 'No se envió ningún archivo'], 400);
+}
 }
--- react-app/src/pages/Profile.jsx
+++ react-app/src/pages/Profile.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
+import "../css/Profile.css";
+import axios from 'axios'; 
+
+import avatarIcon from '../assets/avatar-icon.svg';
+import createIcon from '../assets/create-icon.svg';
 
 export default function Profile() {
-	const { user } = useAuth();
+  const { user } = useAuth();
+  const [avatar, setAvatar] = useState(user.avatar);
+
+  const handleImageChange = async (event) => {
+    const newAvatar = event.target.files[0];
+    console.log('Nueva imagen seleccionada:', newAvatar); 
+    setAvatar(URL.createObjectURL(newAvatar)); 
+
+    const formData = new FormData();
+    formData.append('avatar', newAvatar);
+	try {
+
+		await axios.post('http://localhost:8000/api/update-avatar', formData, {
+		  withCredentials: true,
+		  headers: {
+			'Content-Type': 'multipart/form-data', 
+		  },
+		});
+		console.log('Avatar subido con éxito');
+	  } catch (error) {
+		console.error('Error al subir el avatar:', error);
+	  }
+	};
 	return (
-		<>
-			<h2>User Profile</h2>
-				<div>
-				Name: {user.name}
-				</div>
-				<div>
-				Email: {user.email}
-				</div>
-				
-		</>
-	);
+		<div className="container-profile">
+		  <img className="icon-nav profile" src={avatarIcon} alt="icono perfil" />
+		  <label className="fileInput-create-avatar" htmlFor="fileInput">
+		  <img
+          className="icon-nav profileAvatar"
+          src={createIcon}
+          alt="icono de agregar fotografía"
+        />
+		<input
+          id="fileInput"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+		  />
+      </label>
+      <h2 className="info-perfil title-profile">{user.name}</h2>
+	  <div className="info-perfil">
+        Email: <span>{user.email}</span>
+      </div>
+	  </div>
+  );
 }
